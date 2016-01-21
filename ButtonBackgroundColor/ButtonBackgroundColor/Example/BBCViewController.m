5541e5cad80a9b77c7928a8504d96fad1395e4b1
GabrielMassana <gabrielmassana@gmail.com>
2016-01-21 11:48:56 +0000

    Example Code

--- ButtonBackgroundColor/ButtonBackgroundColor/Example/BBCViewController.m
+++ ButtonBackgroundColor/ButtonBackgroundColor/Example/BBCViewController.m
@@ -8,15 +8,42 @@
 
 #import "BBCViewController.h"
 
+#import "UIButton+BBCBackgroundColor.h"
+
 @interface BBCViewController ()
+
+@property (nonatomic, strong) UIButton *button;
 
 @end
 
 @implementation BBCViewController
 
+#pragma mark - ViewLifecycle
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    [self.view addSubview:self.button];
+}
+
+#pragma mark - Subviews
+
+- (UIButton *)button
+{
+    if (!_button)
+    {
+        _button = [UIButton buttonWithType:UIButtonTypeCustom];
+        _button.frame = CGRectMake(0.0f,
+                                   100.0f,
+                                   CGRectGetWidth([UIScreen mainScreen].bounds),
+                                   80.0f);
+        
+        [_button bbc_backgroundColorNormal:[UIColor redColor]
+                   backgroundColorSelected:[UIColor blueColor]];
+    }
+    
+    return _button;
 }
 
 @end
